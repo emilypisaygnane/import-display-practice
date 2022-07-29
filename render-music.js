7f1413fc439d4f1ec2fff2de7f61df28d6e6e175
Emily <episaygnane@me.com>
2022-07-29 11:23:40 -0700

    fixed my typos. and add my CSS!!!

--- render-music.js
+++ render-music.js
@@ -10,7 +10,7 @@ export function renderMusic(music) {
 
     artistEl.textContent = music.artist;
     albumEl.textContent = music.album;
-    publicationEl.textContent = `Album produced by ${music.publication.label} and released in ${music.publication.released}. `;
+    publicationEl.textContent = `Album produced by ${music.publication.label} and released on ${music.publication.released}. `;
 
     for (let genre of music.genres) {
         const genreEl = document.createElement('li');
@@ -24,7 +24,7 @@ export function renderMusic(music) {
     topEl.classList.add('top');
 
     musicEl.append(topEl, rightEl);
-    topEl.append.add(artistEl, albumEl);
+    topEl.append(artistEl, albumEl);
     rightEl.append(genresEl, publicationEl);
 
     return musicEl;
